2e0b8bb98aa559b897c4e74a762dc0b6c7caed95
webappdevlop2026-oss <webappdevlop2026@gmail.com>
2026-07-30 03:28:29 +0530

    Add files via upload

---
+++ project-request/app-config.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+  agencyName: "Digital Agency by Chandan Das",
+  agencyTagline: "App • Website • iOS • Custom Software",
+  supportPhone: "",
+  supportWhatsApp: "",
+  supportEmail: "",
+
+  /*
+   * Paste the Google Apps Script Web App URL after deployment.
+   * Until then, the portal automatically runs in Demo Mode.
+   */
+  googleSheets: {
+    webAppUrl: "https://script.google.com/macros/s/AKfycbwTkaKdkxTfR40YsvMtMShl2nPjkIA74Kr40_w1FoyH1KutwTpU2iocM-um8r_sq21Gzw/exec"
+  }
+};
