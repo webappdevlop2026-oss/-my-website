accc18da1d7aff23fc8d0df230a74a7a62a30632
webappdevlop2026-oss <webappdevlop2026@gmail.com>
2026-07-31 00:40:49 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* v4.0.6 build:2026-07-31-premium-seo-cards */
-const CACHE_NAME = 'digital-agency-chandan-v406';
+/* v4.0.7 build:2026-07-31-remove-call-section */
+const CACHE_NAME = 'digital-agency-chandan-v407';
 const CORE = [
   '/',
   '/index.html',
