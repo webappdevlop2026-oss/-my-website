e09cc5d4594419db1850430f95bd31cff2b31ed9
webappdevlop2026-oss <webappdevlop2026@gmail.com>
2026-08-02 04:33:36 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='client-room-v29-project-real';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./client-room.html','./project-room-real.png','client-room-gate-v10.png'])))});
+const CACHE='client-room-v30-project-real';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./client-room.html','./project-room-real.png','client-room-gate-v30.png'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./client-room.html')))});
